357c3fa1184828defcccddf19d62bc98bb4bcee7
Jeremy Brenner <jeremy.brenner@asynchrony.com>
2019-07-01 14:38:50 -0500

    made half pill for coloring

--- dr_mario_pill.js
+++ dr_mario_pill.js
@@ -1,16 +1,9 @@
-const r = 1;
-const cw = 2;
-
-function endCap() {
-  return rotate([90,0,0],sphere({r, fn: 50, center:true }));
-}
+const r = 0.5;
 
 function main () {
 
  return union( 
-     translate([0,cw/2,0], endCap() ),
-     rotate([90,0,0],cylinder({r, h: cw, fn: 50, center:true})),
-     translate([0,-cw/2,0], endCap() )
+     rotate([90,0,0],sphere({r: r, fn: 50, center:true })),
+     translate([0,r/2,0],rotate([90,0,0],cylinder({r: r, h: r, fn: 50, center:true})))
      );
 }
-  
