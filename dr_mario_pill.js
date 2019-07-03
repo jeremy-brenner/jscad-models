79684bcaa58c73fd20470b56ce818802c2a4b170
Jeremy Brenner <jeremy.brenner@asynchrony.com>
2019-07-03 12:07:39 -0500

    remove unnecessary rotations for simplicity

--- dr_mario_pill.js
+++ dr_mario_pill.js
@@ -1,9 +1,20 @@
+const fn = 20;
 const r = 0.5;
 
 function main () {
 
- return union( 
-     rotate([90,0,0],sphere({r: r, fn: 50, center:true })),
-     translate([0,r/2,0],rotate([90,0,0],cylinder({r: r, h: r, fn: 50, center:true})))
-     );
+  return union( 
+    rotate(
+      [90,0,0],
+      sphere({r, fn, center:true })
+    ),
+    translate(
+      [0,r/2,0],
+      rotate(
+        [90,0,0],
+        cylinder({r, h: r, fn, center:true})
+      )
+    )
+  );
 }
+  
