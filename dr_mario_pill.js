c48d4ec68bbacb77e6e42927c7d29511019048a1
Jeremy Brenner <jeremy.brenner@asynchrony.com>
2019-07-03 20:28:48 -0500

    simplified pill

--- dr_mario_pill.js
+++ dr_mario_pill.js
@@ -2,19 +2,8 @@ const fn = 20;
 const r = 0.5;
 
 function main () {
-
   return union( 
-    rotate(
-      [90,0,0],
-      sphere({r, fn, center:true })
-    ),
-    translate(
-      [0,r/2,0],
-      rotate(
-        [90,0,0],
-        cylinder({r, h: r, fn, center:true})
-      )
-    )
+    sphere({r, fn, center:true }),
+    translate([0,0,r/2],cylinder({r, h: r, fn, center:true}))
   );
 }
-  
