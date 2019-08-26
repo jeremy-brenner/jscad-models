934cf137c0129a536f8116639787e65cc5f78cd0
Jeremy Brenner <jeremyjbrenner@gmail.com>
2019-08-26 00:13:31 -0500

    add quality setting

--- doc-ock-tentacle.js
+++ doc-ock-tentacle.js
@@ -3,15 +3,14 @@ const hw = 8;
 const ri = 8;
 const ro = 4;
 
-//const fni = 64;
-//const fno = 64;
-const fni = 16;
-const fno = 16;
+const quality = 1;
+
+const fn = 16 * quality;
 
 function main() {
   return difference( 
       humps(),
-      translate([0,0.2,0], scale([0.97, 0.97,1 ], difference(
+      translate([0,0.2,0], scale([0.98, 0.98,1 ], difference(
           humps(), 
            translate([-17.5,-24,-20],cube([35,11,100])),
            translate([-19,30,-20],cube([38,11,100]))
@@ -32,8 +31,8 @@ function humps() {
       translate([0,0,hw],hump()), 
       translate([0,0,hw*2],hump()),
       
-     translate([0,-4,hw],rotate([90,0,0],torus({ ri: 2, ro: 5, fni: fni/2, fno }))),
-      translate([0,-4,hw],rotate([90,0,0],torus({ ri: 2, ro: 2, fni: fni/2, fno })))
+     translate([0,-4,hw],rotate([90,0,0],torus({ ri: 2, ro: 5, fni: fn/2, fno: fn }))),
+      translate([0,-4,hw],rotate([90,0,0],torus({ ri: 2, ro: 2, fni: fn/2, fno: fn })))
 
 
       )
@@ -47,11 +46,11 @@ function hump() {
         intersection(
             translate([-15,-5.5,-hw/2],cube([30,20,hw])),
             union(
-                torus({ ri, ro, fni,fno }),
-                translate([ro,0,0], rotate([90,0,0],cylinder({r: ri, h: 4, fn: fni}))),
-                translate([-ro,0,0], rotate([90,0,0],cylinder({r: ri, h: 4, fn: fni}))),
-                translate([ro,-4,0],rotate([90,0,0],torus({ ri:1.5 , ro: ri-1.5, fni: fni/2, fno: fni }))),
-                translate([-ro,-4,0],rotate([90,0,0],torus({ ri:1.5 , ro: ri-1.5, fni: fni/2, fno: fni })))
+                torus({ ri, ro, fni: fn,fno: fn }),
+                translate([ro,0,0], rotate([90,0,0],cylinder({r: ri, h: 4, fn}))),
+                translate([-ro,0,0], rotate([90,0,0],cylinder({r: ri, h: 4, fn}))),
+                translate([ro,-4,0],rotate([90,0,0],torus({ ri:1.5 , ro: ri-1.5, fni: fn/2, fno: fn }))),
+                translate([-ro,-4,0],rotate([90,0,0],torus({ ri:1.5 , ro: ri-1.5, fni: fn/2, fno: fn })))
             )
         )
       ),
@@ -63,6 +62,6 @@ function hump() {
 function bevel() {
     return difference(
              cube([2,2,40]),
-         cylinder({r: 2, h: 40, fn: fno})
+         cylinder({r: 2, h: 40, fn: fn})
          );
 }
