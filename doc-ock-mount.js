d03c9f8b7faf0cf7aa03a0b21329899ea0be94da
Jeremy Brenner <jeremyjbrenner@gmail.com>
2019-09-29 21:26:04 -0500

    add print size scale

--- doc-ock-mount.js
+++ doc-ock-mount.js
@@ -5,32 +5,22 @@ const inner_height = height-hole_d;
 const small_section_r = hole_r + 5;
 const large_section_r = small_section_r + 2;
 
-const bump_r = large_section_r+25;
-
 const quality = 4;
 
 const fn = 16 * quality;
 
-const w = 10;
+const printSize = 0.95;
+
 function main() {
     const start = Date.now();
-    const model = render();
+    const model = scale([printSize,printSize,printSize],render());
     const runTime = Date.now() - start;
     console.log(runTime/1000);
     return model;
 }
 
 function render() {
-  return union(
-    mount()
-  //   hump()
- 
-
-      );
-}
-
-function mountHump() {
-    return union( mount(), hump() );
+  return mount();
 }
 
 function mount() {
@@ -48,16 +38,6 @@ function mount() {
         );
 }
 
-function hump() {
-          return difference(
-      intersection(
-          translate([0,0,-25],sphere({r:bump_r,fn})),
-          translate([-bump_r,-bump_r,0],cube([bump_r*2,bump_r*2,height/2-2]))
-          ),
-          cylinder({r:large_section_r+2, h:height/2, fn})
-          )
-}
-
 function hole() {
   return translate([0,0,inner_height],intersection( 
     cylinder({r:hole_r, h:hole_d, fn}),
@@ -65,13 +45,15 @@ function hole() {
   ));
 }
 
-
 function flanges() {
-   return translate([0,0,inner_height], difference(
-  translate([-33,-36,0],cube([66,72,30])),
-translate([-33,-12,18],rotate([0,90,0],cylinder({r: 5, h: 66, fn}))),
-translate([-31,-17,18],cube([62,10,12])),
-translate([-30,-40,0],cube([60,80,60])),
-translate([7,45,30],rotate([0,90,0],cylinder({r: 5, h: 66, fn})))         
-   ));
+    return translate([0,0,inner_height], 
+        difference(
+            translate([-33,-36,0],cube([66,72,30])),
+            translate([-33,-12,18],rotate([0,90,0],cylinder({r: 5, h: 66, fn}))),
+            translate([-31,-17,18],cube([62,10,12])),
+            translate([-30,-40,0],cube([60,80,60])),
+            translate([7,45,30],rotate([0,90,0],cylinder({r: 5, h: 66, fn})))         
+        )
+   );
 }
+
